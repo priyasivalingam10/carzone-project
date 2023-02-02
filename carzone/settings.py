ef77b0cde7e6ae4dc3fec4d65ac3065c6139da9a
priyasivalingam10 <priyaran0310@gmail.com>
2023-02-02 21:30:31 +0530

    created models

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER':'postgres',
+        'PASSWORD':'123',
+        'HOST':'localhost'
     }
 }
 
@@ -112,7 +115,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -123,3 +126,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'carzone/static')
 ]
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    first_name=models.CharField( max_length=255,blank='False')
+    last_name=models.CharField( max_length=255,blank='False')
+    designation=models.CharField( max_length=255,blank='False')
+    photo=models.ImageField( upload_to='photo/%Y/%m/%d/')
+    facebook_link=models.CharField( max_length=255,blank='False')
+    twitter_link=models.CharField( max_length=255,blank='False')
+    google_plus_link=models.CharField( max_length=255,blank='False')
+    created_date=models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.first_name
